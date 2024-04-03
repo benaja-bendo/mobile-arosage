79e83a9246a75ad6939f733399fe73435d1ab7c1
RichardKoudou <richard.koudou@epsi.fr>
2024-04-04 00:32:29 +0200

    implementation homePage + fetch API

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,17 +1,67 @@
 import { StyleSheet } from 'react-native';
-
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import React, {useEffect, useState} from 'react';
+import {ActivityIndicator, FlatList} from 'react-native';
 
-export default function TabOneScreen() {
+//export default function TabOneScreen() {
+  
+type Data = {
+  id: string;
+  title: string;
+  description: string;
+  created_at: string;
+};
+
+const App = () => {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState<Data[]>([]);
+
+  const getData = async () => {
+    try {
+      const response = await fetch('https://acc0-2a02-8428-ed77-e101-54ba-6f5b-710d-95e9.ngrok-free.app/api/plants');
+      const json = await response.json();
+      setData(json.data);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+    <EditScreenInfo path="app/(tabs)/index.tsx" />
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : (
+        <FlatList
+          data={data}
+          keyExtractor={({id}) => id}
+          renderItem={({item}) => (
+            <Text>
+              {item.title}, {item.description}, {item.created_at}
+            </Text>
+          )}
+        />
+      )}
     </View>
   );
-}
+};
+
+export default App;
+  /*return (
+    
+      <Text style={styles.title}>Tab One</Text>
+      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      
+    </View>
+  );
+}*/
 
 const styles = StyleSheet.create({
   container: {
