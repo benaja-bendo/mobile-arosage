871938a1c33fbce75dd2aaa28fa14b7894999f2b
emmarft <emma.ruffet@epsi.fr>
2024-04-07 09:43:08 +0200

    First commit

--- app/(tabs)/advice.tsx
+++ app/(tabs)/advice.tsx
@@ -1,30 +1,48 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, TextInput } from 'react-native';
+import React, { useState } from 'react';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
 
 export default function TabAdviceScreen() {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const handleSearch = (text: string) => {
+    setSearchQuery(text);
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab Advice</Text>
+      <TextInput
+        style={styles.searchInput}
+        placeholder="Recherche..."
+        value={searchQuery}
+        onChangeText={handleSearch}
+      />
       <Text style={styles.title}>Hello</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    title: {
-      fontSize: 20,
-      fontWeight: 'bold',
-    },
-    separator: {
-      marginVertical: 30,
-      height: 1,
-      width: '80%',
-    },
-  });
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  searchInput: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 8,
+    paddingHorizontal: 10,
+    marginTop: 20,
+    marginBottom: 10,
+    width: '80%',
+  },
+});
